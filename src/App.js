c7e413ea70b244fc748166da55bf092fe4879db9
Gabriela Barcena <g.barcena.v@gmail.com>
2019-10-28 16:34:54 -0600

    Se agregan archivos de Firebase, webpack y cambio unregistrer por registrer

--- src/App.js
+++ src/App.js
@@ -6,16 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>¡Hola mundo!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Holi</h1>
       </header>
     </div>
   );
