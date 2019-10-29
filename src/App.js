5d694255c4aa5368b37030273a88e7effce63965
Gabriela Barcena <g.barcena.v@gmail.com>
2019-10-28 18:48:57 -0600

    Se agrega librería para estilos (aphrodite), se hicieron puebas para estilos

--- src/App.js
+++ src/App.js
@@ -1,63 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/registry">Registry</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/feed">Feed</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
+import Home from "./views/Welcome";
+import Registry from "./views/Registry";
+import Login from "./views/Login";
+import Feed from "./views/Feed";
+import Profile from "./views/Profile";
 
-        <Switch>
-          <Route path="/registry">
-            <Registry />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/feed">
-            <Feed />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/registry">Registry</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/feed">Feed</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path="/registry" Route={Registry} />
+
+            <Route path="/login" Route={Login} />
+
+            <Route path="/feed" Route={Feed} />
+
+            <Route path="/profile" Route={Profile} />
+
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
-function Home() {
-  return <h1>HOME</h1>;
-}
-function Registry() {
-  return <h1>REGISTRO</h1>;
-}
-function Login() {
-  return <h1>INICIO DE SESIÓN</h1>;
-}
-function Feed() {
-  return <h1>FEED</h1>;
-}
-function Profile() {
-  return <h1>PERFIL</h1>;
-}
+export default App;
