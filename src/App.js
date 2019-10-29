6b6979da6ebccb9e6758e31c6b3dfddcf5771a60
Gabriela Barcena <g.barcena.v@gmail.com>
2019-10-29 16:18:39 -0600

    Se agregaron archivo de configuración de Firebase y autenticación

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import fire from "./configuration/firebase";
 
 import Home from "./views/Home/Welcome";
 import Registry from "./views/Registry/Registry";
@@ -9,8 +10,33 @@ import Feed from "./views/Feed";
 import Profile from "./views/Profile";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+    this.authListener = this.authListener.bind(this);
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged(user => {
+      console.log(user);
+      if (user) {
+        this.setState({ user });
+        localStorage.setItem("user", user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem("user");
+      }
+    });
+  }
   render() {
     return (
+      // <div>{this.state.user ? : (<Home/>) : (<Login/>)} </div>
       <Router>
         <Switch>
           <Route path="/registry" component={Registry} />
