4771454b855510461a81da68198e277a1531f7f7
Gabriela Barcena <g.barcena.v@gmail.com>
2019-10-29 16:15:32 -0600

    Updates

---
+++ precache-manifest.256c6810921daf07831416df2597d673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73eb161ac51802630f0d48089b49e08d",
+    "url": "/gbarcenav/social-networkPA.git/index.html"
+  },
+  {
+    "revision": "0fd784a5d3c215882ed0",
+    "url": "/gbarcenav/social-networkPA.git/static/css/main.b8563b82.chunk.css"
+  },
+  {
+    "revision": "6a1d804f531288c09460",
+    "url": "/gbarcenav/social-networkPA.git/static/js/2.29471e96.chunk.js"
+  },
+  {
+    "revision": "0fd784a5d3c215882ed0",
+    "url": "/gbarcenav/social-networkPA.git/static/js/main.b3c91b90.chunk.js"
+  },
+  {
+    "revision": "e8a2cae6236364347b46",
+    "url": "/gbarcenav/social-networkPA.git/static/js/runtime-main.f7f5503b.js"
+  },
+  {
+    "revision": "667e6e56f929752b062d3938c25facc0",
+    "url": "/gbarcenav/social-networkPA.git/static/media/previous-alt.667e6e56.svg"
+  }
+]);
